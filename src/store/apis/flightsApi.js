08b97e5b85245bc408746b2c51e26654e16915d2
mertaydin <69799486+mertaydinsololearn@users.noreply.github.com>
2024-01-09 00:45:50 +0300

    Update flightsApi.js

--- src/store/apis/flightsApi.js
+++ src/store/apis/flightsApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const flightsApi = createApi({
 	reducerPath: 'flights',
 	baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:3005',
+        baseUrl: 'https://amadeusflight-ff6f18e173db.herokuapp.com:3005',
 		fetchFn: async (...args) => {
 			return fetch(...args);
 		}
